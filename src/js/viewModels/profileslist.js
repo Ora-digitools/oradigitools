a8358970ff7636c5e92d46fecef91efb935a6a18
ashrithamalli <ashritha.malli@gmail.com>
2017-07-24 23:27:13 +0530

    updated filter and gallery

--- src/js/viewModels/profileslist.js
+++ src/js/viewModels/profileslist.js
@@ -11,30 +11,25 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojknockout', '
 
     function DashboardViewModel() {
       var self = this;
-      var selectedLocations = "", selectedSkills = "", selectedPillers = "";
-      self.url = "http://digital-db.us.oracle.com:7003/ords/seaas_stage/seaas/GetUserProfiles";
+      var selectedLocations = "", selectedPillars = "";
+      self.url = baseurl + "GetUserProfiles";
       self.next = "";
       self.prev = "";
       self.data = ko.observableArray();
       self.renderData = ko.observableArray();
       self.currentItemId = ko.observable();
-      self.keywords = ko.observableArray(['employee_key', 'u', 'm', 'uuid', 'first_name',
-        'last_name', 'display_name', 'title', 'work_phone', 'mobile_phone', 'work_email',
-        'city', 'state', 'local_time', 'created', 'created_by', 'last_updated', 'last_updated_by',
-        'ou', 'cost_center', 'target_dn', 'country', 'manager_email', 'profile_photo_id',
-        'profile_photo_url', 'skills', 'customers', 'category', 'value', 'desc_text',]);
 
       self.hubslist = ko.observableArray([]);
       self.listofpillers = ko.observableArray([]);
-      self.selectechubs=ko.observableArray([]);
+      self.searchtext = ko.observable('');
 
-       self.openthefilterpanel = function () {		
+      self.openthefilterpanel = function () {
         $('#slider').addClass('open');
         $('#fixb').addClass('close');
       }
 
       self.closethefilterpanel = function () {
-		  $('#slider').removeClass('open');
+        $('#slider').removeClass('open');
         $('#fixb').removeClass('close');
       }
 
@@ -49,51 +44,103 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojknockout', '
       }
 
       this.searchhandler = function (context, ui) {
-        var searchkey = ui.value.length > 0 ? ui.value[0].toLowerCase() : "";
+        self.closethefilterpanel();
+        var searchkey = self.searchtext() != "" ? self.searchtext()[0] : "";//;
 
-        if (searchkey != "") {
-          $.getJSON(baseurl + "ords/seaas_stage/seaas/SearchUserProfiles/" + searchkey).
-            then(function (profiles) {
-              console.log(">> " + profiles.items.length);
-              self.data([]);
-              self.renderData([]);
-              $.each(profiles.items, function () {
-
-                var imageurl = 'https://raw.githubusercontent.com/Ora-digitools/oradigitools/master/UI_Assets/Profile-list-page/default-user-icon.png';
-                if (!this.profile_photo_url.endsWith("GetPhoto/")) {
-                  imageurl = this.profile_photo_url;
-                }
-                var profilejson = stripkeys(JSON.stringify(this));
-                var profile = {
-                  icon: imageurl,
-                  name: this.display_name,
-                  title: this.title,
-                  work_email: this.work_email,
-                  work_phone: this.work_phone,
-                  mobile_phone: this.mobile_phone,
-                  city: this.city,
-                  state: this.state,
-                  country: this.country,
-                  uuid: this.uuid,
-                  json: profilejson
-                };
-                if (this.work_email != 'matthew.orsie@oracle.com') {
-                  self.data.push(profile);
-                  self.renderData.push(profile);
-                }
-                console.log("Parse completed");
-              });
-
-            });
-        } else {
-          self.getUserList();
+        var searchheader = {
+          'search_terms': searchkey,
+          'pillar': selectedPillars,
+          'hub': selectedLocations
         }
 
+        console.log(searchheader);
+        $.ajax({
+          url: baseurl + 'SearchUserProfiles',
+          cache: false,
+          beforeSend: function (xhr) {
+            xhr.setRequestHeader('Access-Control-Allow-origin', 'http://solutionengineering.us.oracle.com');
+            xhr.setRequestHeader('search_terms', searchkey);
+            xhr.setRequestHeader('pillar',selectedPillars);
+            xhr.setRequestHeader('hub',selectedLocations);
+          },
+          type: 'GET',
+          crossDomain: true,
+          success: function (profiles) {
+            console.log(">> " + profiles.items.length);
+            self.data([]);
+            self.renderData([]);
+            $.each(profiles.items, function () {
 
+              var imageurl = 'https://raw.githubusercontent.com/Ora-digitools/oradigitools/master/UI_Assets/Profile-list-page/default-user-icon.png';
+              if (!this.profile_photo_url.endsWith("GetPhoto/")) {
+                imageurl = this.profile_photo_url;
+              }
+              var profilejson = stripkeys(JSON.stringify(this));
+              var profile = {
+                icon: imageurl,
+                name: this.display_name,
+                title: this.title,
+                work_email: this.work_email,
+                work_phone: this.work_phone,
+                mobile_phone: this.mobile_phone,
+                city: this.city,
+                state: this.state,
+                country: this.country,
+                uuid: this.uuid,
+                json: profilejson
+              };
+              if (this.work_email != 'matthew.orsie@oracle.com') {
+                self.data.push(profile);
+                self.renderData.push(profile);
+              }
+              console.log("Parse completed");
+            });
+
+          }
+        }).fail(function (xhr, textStatus, err) {
+          alert(err);
+        });
+
+        // if (searchkey != "") {
+        //   $.getJSON(baseurl + "SearchUserProfiles/" + searchkey).
+        //     then(function (profiles) {
+        //       console.log(">> " + profiles.items.length);
+        //       self.data([]);
+        //       self.renderData([]);
+        //       $.each(profiles.items, function () {
+
+        //         var imageurl = 'https://raw.githubusercontent.com/Ora-digitools/oradigitools/master/UI_Assets/Profile-list-page/default-user-icon.png';
+        //         if (!this.profile_photo_url.endsWith("GetPhoto/")) {
+        //           imageurl = this.profile_photo_url;
+        //         }
+        //         var profilejson = stripkeys(JSON.stringify(this));
+        //         var profile = {
+        //           icon: imageurl,
+        //           name: this.display_name,
+        //           title: this.title,
+        //           work_email: this.work_email,
+        //           work_phone: this.work_phone,
+        //           mobile_phone: this.mobile_phone,
+        //           city: this.city,
+        //           state: this.state,
+        //           country: this.country,
+        //           uuid: this.uuid,
+        //           json: profilejson
+        //         };
+        //         if (this.work_email != 'matthew.orsie@oracle.com') {
+        //           self.data.push(profile);
+        //           self.renderData.push(profile);
+        //         }
+        //         console.log("Parse completed");
+        //       });
+        //     });
+        // } else {
+        //   self.getUserList();
+        // }
       }
 
       self.getFilters = function () {
-        $.getJSON("http://digital-db.us.oracle.com:7003/ords/seaas_stage/seaas/ListValues/SOLUTION_HUBS").
+        $.getJSON(baseurl + "ListValues/SOLUTION_HUBS").
           then(function (hubs) {
             $.each(hubs.items, function () {
               console.log(this.value);
@@ -104,7 +151,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojknockout', '
             })
           });
 
-        $.getJSON("http://digital-db.us.oracle.com:7003/ords/seaas_stage/seaas/ListValues/ENGAGEMENT_PILLAR").
+        $.getJSON(baseurl + "ListValues/ENGAGEMENT_PILLAR").
           then(function (pillers) {
             $.each(pillers.items, function () {
               console.log(this.value);
@@ -122,8 +169,8 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojknockout', '
             console.log(">> " + profiles.items.length);
             self.data([]);
             self.renderData([]);
-            self.next = profiles.next!=undefined?profiles.next.$ref:'';
-            self.prev = profiles.previous!=undefined?profiles.previous.$ref:'';
+            self.next = profiles.next != undefined ? profiles.next.$ref : '';
+            self.prev = profiles.previous != undefined ? profiles.previous.$ref : '';
             $.each(profiles.items, function () {
 
               var imageurl = 'https://raw.githubusercontent.com/Ora-digitools/oradigitools/master/UI_Assets/Profile-list-page/default-user-icon.png';
@@ -153,10 +200,27 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojknockout', '
           });
       }
 
-      self.applyFilter=function(){
-        console.log(self.selectechubs);
+      filteredhubs = function (desc) {
+
+        if (desc.checked) {
+          selectedLocations += selectedLocations.length > 0 ? ',' + desc.defaultValue : desc.defaultValue;
+        } else {
+          selectedLocations = selectedLocations.replace(desc.defaultValue + ',', '');
+          selectedLocations = selectedLocations.replace(',' + desc.defaultValue, '');
+          selectedLocations = selectedLocations.replace(desc.defaultValue, '');
+        }
       }
 
+      filteredpillars = function (desc) {
+
+        if (desc.checked) {
+          selectedPillars += selectedPillars.length > 0 ? ',' + desc.defaultValue : desc.defaultValue;
+        } else {
+          selectedPillars = selectedPillars.replace(desc.defaultValue + ',', '');
+          selectedPillars = selectedPillars.replace(',' + desc.defaultValue, '');
+          selectedPillars = selectedPillars.replace(desc.defaultValue, '');
+        }
+      }
       self.loadNextPage = function () {
         self.url = self.next;
         self.getUserList();
@@ -167,7 +231,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojknockout', '
         self.getUserList();
       }
       self.loadFirstPage = function () {
-        self.url = "http://digital-db.us.oracle.com:7003/ords/seaas_stage/seaas/GetUserProfiles";
+        self.url = baseurl + "GetUserProfiles";
         self.getUserList();
       }
 
